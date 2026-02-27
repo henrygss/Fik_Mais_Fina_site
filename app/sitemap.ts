970d06d1a84d92d8d13a109b659b725072cedd2e
Henry <henry17042002@gmail.com>
2026-02-27 16:20:46 -0300

    feat: add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://fik-mais-fina-site.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fik-mais-fina-site.vercel.app/produtos",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fik-mais-fina-site.vercel.app/produtos/produto-1",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fik-mais-fina-site.vercel.app/produtos/produto-2",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://fik-mais-fina-site.vercel.app/produtos/produto-3",
+      lastModified: new Date(),
+    },
+  ];
+}
